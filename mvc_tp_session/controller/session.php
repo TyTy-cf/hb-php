c9e1e5559329cdaac388562d25c533cd430a2dd1
TOURRET Kevin <tourret.kevin@gmail.com>
2021-06-24 11:36:17 +0200

    tp exo regions

--- mvc_tp_session/controller/session.php
+++ mvc_tp_session/controller/session.php
@@ -3,3 +3,13 @@
 if (session_id() == '') {
     session_start();
 }
+
+$file = 'https://pokeapi.co/api/v2/pokemon';
+$data = file_get_contents($file);
+print_r($data);
+echo '<br>';
+foreach(json_decode($data) as $key => $value) {
+    echo '<br>';
+    print_r($key);
+    print_r($value);
+}
