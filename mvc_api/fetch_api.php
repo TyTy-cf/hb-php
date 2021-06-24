139dc09e7af8eb89ac7b5c720038bec0b3748872
TOURRET Kevin <tourret.kevin@gmail.com>
2021-06-24 11:39:00 +0200

    add tp region

--- mvc_api/fetch_api.php
+++ mvc_api/fetch_api.php
@@ -1,1 +1,11 @@
 <?php
+
+$file = 'https://pokeapi.co/api/v2/pokemon/1/';
+$data = file_get_contents($file);
+print_r($data);
+echo '<br>';
+foreach(json_decode($data) as $key => $value) {
+    echo '<br>';
+    print_r($key);
+    print_r($value);
+}
